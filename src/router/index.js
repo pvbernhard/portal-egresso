17bf635fe399379654a1c8f64479f6a4c31551ae
Pedro Bernhard <pvbernhard@gmail.com>
2022-06-15 14:48:37 -0300

    add: rotas

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHistory } from 'vue-router';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -7,17 +6,34 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: HomeView
+      component: () => import('../pages/PaginaInicial.vue'),
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
-  ]
-})
+      path: '/egressos',
+      name: 'egressos',
+      component: () => import('../pages/EgressosPage.vue'),
+    },
+    {
+      path: '/depoimentos',
+      name: 'depoimentos',
+      component: () => import('../pages/DepoimentosPage.vue'),
+    },
+    {
+      path: '/cadastro',
+      name: 'cadastro',
+      component: () => import('../pages/CadastroPage.vue'),
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: () => import('../pages/LoginPage.vue'),
+    },
+    {
+      path: '/perfil',
+      name: 'perfil',
+      component: () => import('../pages/PerfilPage.vue'),
+    },
+  ],
+});
 
-export default router
+export default router;
