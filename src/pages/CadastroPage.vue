490e412006ed31e9f307a0474a352182c51c5f46
Pedro Bernhard <pvbernhard@gmail.com>
2022-06-15 14:48:06 -0300

    add: página cadastro

---
+++ src/pages/CadastroPage.vue
@@ -0,0 +1,59 @@
+<template>
+<div class="cadastro m-6">
+  <h1 class="is-size-2 my-2">Cadastro</h1>
+  <div class="field">
+    <label class="label">Nome</label>
+    <div class="control">
+      <input class="input" type="text" placeholder="Nome">
+    </div>
+  </div>
+  <div class="field">
+    <label class="label">E-mail</label>
+    <div class="control">
+      <input class="input" type="text" placeholder="nome@email.com">
+    </div>
+  </div>
+  <div class="field">
+    <label class="label">CPF</label>
+    <div class="control">
+      <input class="input" type="text" placeholder="XXX.XXX.XXX-XX">
+    </div>
+  </div>
+  <div class="field">
+    <label class="label">Profissão</label>
+    <div class="control">
+      <input class="input" type="text" placeholder="Sua profissão">
+    </div>
+  </div>
+  <div class="field">
+    <label class="label">Cargo</label>
+    <div class="control">
+      <input class="input" type="text" placeholder="Seu cargo">
+    </div>
+  </div>
+  <div class="field">
+    <label class="label">Curso</label>
+    <div class="control">
+      <input class="input" type="text" placeholder="Seu curso">
+    </div>
+  </div>
+  <div class="field">
+    <label class="label">Contato</label>
+    <div class="control">
+      <input class="input" type="text" placeholder="(XX) XXXXX-XXXX">
+    </div>
+  </div>
+  <div class="control">
+    <button class="button is-info">Cadastrar</button>
+  </div>
+</div>
+
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>
