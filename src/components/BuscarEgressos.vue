14b7533132812f77603ab7892f5bd0a904a351a5
Pedro Bernhard <pvbernhard@gmail.com>
2022-06-15 14:46:32 -0300

    add: buscar egressos

---
+++ src/components/BuscarEgressos.vue
@@ -0,0 +1,22 @@
+<template>
+<div class="field">
+  <label class="label">Buscar Egresso</label>
+  <div class="control">
+    <input class="input" type="text" placeholder="Pesquise aqui ...">
+  </div>
+
+  <div class="control py-3">
+    <button class="button is-white">Pesquisar</button>
+  </div>
+</div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+.label {
+  color: white;
+}
+</style>
